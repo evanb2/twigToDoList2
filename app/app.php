de8e25064cf148b3b4f05bae231898d59d1265de
Evan Butler & tommy bountham <student@epicodus.com>
2015-03-16 15:57:27 -0700

    debug twig files and app.php file

--- app/app.php
+++ app/app.php
@@ -1,6 +1,7 @@
 <?php
     require_once __DIR__."/../vendor/autoload.php";
     require_once __DIR__."/../src/Task.php";
+    require_once __DIR__."/../src/Category.php";
 
     $app = new Silex\Application();
 
@@ -11,19 +12,37 @@
     ));
 
     $app->get("/", function() use ($app) {
+        return $app['twig']->render('index.twig');
+    });
 
+    $app->get("/tasks", function() use ($app) {
         return $app['twig']->render('tasks.twig', array('tasks' => Task::getAll()));
+    });
+
+    $app->get("/categories", function() use ($app) {
+        return $app['twig']->render('categories.twig', array('categories' => Category::getAll()));
+    });
+
+    $app->post("/categories", function() use ($app) {
+        $category = new Category($_POST['name']);
+        $category->save();
+        return $app['twig']->render('categories.twig', array('categories' => Category::getAll()));
     });
 
     $app->post("/tasks", function() use ($app) {
         $task = new Task($_POST['description']);
         $task->save();
-        return $app['twig']->render('create_task.twig', array('newtask' => $task));
+        return $app['twig']->render('tasks.twig', array('tasks' => Task::getAll()));
     });
 
     $app->post("/delete_tasks", function() use ($app) {
         Task::deleteAll();
-        return $app['twig']->render('delete_tasks.twig');
+        return $app['twig']->render('index.twig');
+    });
+
+    $app->post("/delete_categories", function() use ($app) {
+        Category::deleteAll();
+        return $app['twig']->render('index.twig');
     });
 
     return $app;
