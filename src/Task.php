3d3df58b83d3e954a2b77ebbf2142a1693c50084
Evan Butler & tommy bountham <student@epicodus.com>
2015-03-17 10:56:46 -0700

    Add comment to app and task files

--- src/Task.php
+++ src/Task.php
@@ -3,28 +3,28 @@
     {
         private $description;
         private $id;
-
+        //setting a construct for Task class with arguments $description and $id
         function __construct($description, $id = null)
         {
             $this->description = $description;
             $this->id = $id;
         }
-
+        //setting $new_id as an integer and equal to our $id
         function setId($new_id)
         {
             $this->id = (int) $new_id;
         }
-
+        //setting $new_description as a string and equal to our $description
         function setDescription($new_description)
         {
             $this->description = (string) $new_description;
         }
-
+        //getter for returning $id
         function getId()
         {
             return $this->id;
         }
-
+        //getter for returning $description
         function getDescription()
         {
             return $this->description;
@@ -32,40 +32,53 @@
 
         function save()
         {
+            //setting variable using GLOBALS to call tasks table and insert data and return id.
             $statement = $GLOBALS['DB']->query("INSERT INTO tasks (description) VALUES ('{$this->getDescription()}') RETURNING id;");
+            //use PDO fetch method to get id and put into assoc array
             $result = $statement->fetch(PDO::FETCH_ASSOC);
+            //setting result to setId
             $this->setId($result['id']);
         }
 
+
         static function getAll()
         {
+            //calling Global variable DB, select all from tasks table, put into $returned_tasks
             $returned_tasks = $GLOBALS['DB']->query("SELECT * FROM tasks;");
+            //creating empty array
             $tasks = array();
+            //foreach loop
             foreach($returned_tasks as $task) {
+                //getting description and id from each task
                 $description = $task['description'];
                 $id = $task['id'];
+                //instantiate new Task with description and id from above in $new_task
                 $new_task = new Task($description, $id);
+                //push everything into tasks array
                 array_push($tasks, $new_task);
             }
+            //return tasks array
             return $tasks;
         }
 
         static function deleteAll()
         {
-           $GLOBALS['DB']->exec("DELETE FROM tasks *;");
+            //use global function to call DB, then exec to run a SQL statement to delete ALL from tasks table
+            $GLOBALS['DB']->exec("DELETE FROM tasks *;");
         }
 
-        static function find($search_id)
+        static function find($search_id)//giving find method search_id as an argument
         {
             $found_task = null;
+            //use getAll method on Task class, save to tasks variable
             $tasks = Task::getAll();
             foreach($tasks as $task) {
-                $task_id = $task->getId();
-                if ($task_id == $search_id) {
-                    $found_task = $task;
+                $task_id = $task->getId();//getting all task id's = new task_id variable
+                if ($task_id == $search_id) {//if they are equal...
+                    $found_task = $task;//set task == new variable found_task
                 }
             }
-            return $found_task;
+            return $found_task;//return found task
         }
     }
 ?>
